7fc6589592d8e847291d1baa77042c2b9be0605e
AlexTreant <9605299@gmail.com>
2022-12-11 15:04:23 +0500

    Поменяли функцию

--- pp.py
+++ pp.py
@@ -1,4 +1,17 @@
-n = 10
-while n > 0:
-    n -= 1
-    print(n)
+def compare(word1, word2, register=True, spaces=True):
+    if register == False and spaces == False:
+        return word1.upper().strip() == word2.upper().strip()
+    elif register == True and spaces == False:
+        return word1.strip() == word2.strip()
+    elif register == False and spaces == True:
+        return word1.upper() == word2.upper()
+    else:
+        return word1 == word2
+
+
+s1 = '  Treant   '
+s2 = 'treant'
+compare(s1, s2)
+compare(s1, s2, register=False)
+compare(s1, s2, spaces=False)
+compare(s1, s2, register=False, spaces=False)
