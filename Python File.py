5cf877ce34005dd594b7774d128ca1642a3c095b
AlexTreant <9605299@gmail.com>
2022-12-11 14:47:16 +0500

    2

--- Python File.py
+++ Python File.py
@@ -5,11 +5,11 @@ def get_nod(a, b):
     """
     Нахождение наибольшего общего делителя из {a} и {b}
     """
-    while a != b:
+    while a != 0:
         if a < b:
             a, b = b, a
-        a -= b
-    return a
+        a %= b
+    return b
 
 # Тест 1
 def test1(func):
@@ -33,11 +33,11 @@ def test2(func):
 
 # Тест 3
 def test3(func):
-    a, b = 1, 90000000
+    a, b = 15, 121050
     start_timer = time.time()
     res = func(a, b)
     end_timer = time.time()
-    if res == 1 and end_timer - start_timer < 1:
+    if res == 15 and end_timer - start_timer < 1:
         print('test 3 - ok')
     else:
         print('test 3 - failed')
